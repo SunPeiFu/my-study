0cb909ed1fec9d74336d178e0e73843d38f79763
sunpeifu <sunpeifu@leyue100.com>
2020-09-17 17:01:14 +0800

    提交

---
+++ src/main/java/com/sunpeifu/study/StudyApplication.java
@@ -0,0 +1,13 @@
+package com.sunpeifu.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyApplication.class, args);
+    }
+
+}
